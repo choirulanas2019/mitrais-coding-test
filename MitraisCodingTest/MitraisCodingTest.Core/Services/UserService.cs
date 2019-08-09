65cb90841e30be5030e5b3fc63adbc43fb446c26
choirulanas2019 <choirulanas.skom@gmail.com>
2019-08-09 23:03:00 +0700

    Add validation for Date Save Date of Birth to table

--- MitraisCodingTest/MitraisCodingTest.Core/Services/UserService.cs
+++ MitraisCodingTest/MitraisCodingTest.Core/Services/UserService.cs
@@ -4,6 +4,7 @@ using MitraisCodingTest.Core.Repositories.Interface;
 using MitraisCodingTest.Core.Services.Interface;
 using MitraisCodingTest.Core.Services.Request;
 using PhoneNumbers;
+using System;
 
 namespace MitraisCodingTest.Core.Services
 {
@@ -29,6 +30,7 @@ namespace MitraisCodingTest.Core.Services
                 Gender = request.Gender,
                 Lastname = request.Lastname,
                 MobileNumber = GetNationalNumber(request.MobileNumber),
+                DateOfBirth = GetDate(request)
             };
 
             _userRepository.Add(user);
@@ -36,20 +38,34 @@ namespace MitraisCodingTest.Core.Services
 
         private void ValidateToAdd(UserRequest request)
         {
-            if (!IsValidDateOfBirth(request)) throw new InvalidValueException("Invalid Date");
-            if (_userRepository.IsExistByEmail(request.Email)) throw new ItemExistException("Email should be unique");
             if (!IsValidMobilePhone(request.MobileNumber)) throw new InvalidValueException("Invalid mobile number");
             if (_userRepository.IsExistByMobileNumber(GetNationalNumber(request.MobileNumber))) throw new ItemExistException("Mobile number should be unique");
+            if (!IsValidDateRequest(request)) throw new InvalidValueException("Invalid Date");
+            if (_userRepository.IsExistByEmail(request.Email)) throw new ItemExistException("Email should be unique");
         }
 
-        private bool IsValidDateOfBirth(UserRequest request)
+        private bool IsValidDateRequest(UserRequest request)
         {
             if (request.Year == ZERO && request.Month == ZERO && request.Date == ZERO)
             {
                 return true;
             }
 
-            return !(request.Year == ZERO || request.Month == ZERO || request.Date == ZERO);
+            if (request.Year == ZERO || request.Month == ZERO || request.Date == ZERO)
+            {
+                return false;
+            }
+
+            try
+            {
+                var dateText = $"{request.Month}/{request.Date}/{request.Year}";
+                Convert.ToDateTime(dateText);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         private bool IsValidMobilePhone(string mobileNumber)
@@ -64,6 +80,12 @@ namespace MitraisCodingTest.Core.Services
             var phoneNumberUtil = PhoneNumberUtil.GetInstance();
             var phoneNumber = phoneNumberUtil.Parse(mobileNumber, "ID");
             return phoneNumber.NationalNumber.ToString();
+        }
+
+        private DateTime GetDate(UserRequest request)
+        {
+            var dateText = $"{request.Month}/{request.Date}/{request.Year}";
+            return Convert.ToDateTime(dateText);
         }
     }
 }
