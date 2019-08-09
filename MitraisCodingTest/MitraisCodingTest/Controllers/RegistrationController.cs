63b612769886bd38e634a394403f24ad915bf010
choirulanas2019 <choirulanas.skom@gmail.com>
2019-08-09 19:54:29 +0700

    Create Initial Project (MVC & Unit Testing) Create RegistrationController, Model, and View Install Entity Framework

---
+++ MitraisCodingTest/MitraisCodingTest/Controllers/RegistrationController.cs
@@ -0,0 +1,37 @@
+﻿using MitraisCodingTest.Models;
+using System.Web.Mvc;
+
+namespace MitraisCodingTest.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public ActionResult Create()
+        {
+            var registrationModel = new RegistrationModel();
+            registrationModel.Gender = "Male";
+
+            return View(registrationModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(RegistrationModel model)
+        {
+            if (!IsValidDateOfBirth(model))
+            {
+                ModelState.AddModelError(string.Empty, "Date of Birth is invalid");
+            }
+
+            return View(model);
+        }
+
+        private bool IsValidDateOfBirth(RegistrationModel model)
+        {
+            if (model.Year == "0" && model.Month == "0" && model.Date == "0")
+            {
+                return true;
+            }
+
+            return !(model.Year == "0" || model.Month == "0" || model.Date == "0");
+        }
+    }
+}
---
+++ MitraisCodingTest/MitraisCodingTest/Models/RegistrationModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MitraisCodingTest.Models
+{
+    public class RegistrationModel
+    {
+        [Required(ErrorMessage = "Mobile number is required")]
+        public string MobileNumber { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string Lastname { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public string Month { get; set; }
+
+        public string Date { get; set; }
+
+        public string Year { get; set; }
+
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email")]
+        public string Email { get; set; }
+    }
+}
